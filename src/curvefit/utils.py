0c0d02ad22a2c7bc101d130a90c6f5bb4c7dc700
zhengp0 <zhengp@uw.edu>
2020-03-31 13:31:30 -0700

    update: add the data translator

--- src/curvefit/utils.py
+++ src/curvefit/utils.py
@@ -266,3 +266,64 @@ def condense_residual_matrix(matrix, sequential_diffs, data_density):
     # return the results for the residual matrix as a (len(available_times), 3) shaped matrix
     r_matrix = np.vstack([far_out, num_data, robs]).T
     return r_matrix
+
+
+def data_translator(data, input_space, output_space,
+                    threshold=1e-16):
+    """Data translator, move data from one space to the other.
+
+    Args:
+        data (np.ndarray): data matrix or vector
+        input_space (str | callable): input data space.
+        output_sapce (str | callable): output data space.
+        threshold (float, optional):
+            Thresholding for the number below 0 in the linear space.
+
+    Returns:
+        np.ndarray:
+            translated data.
+    """
+    if callable(input_space):
+        input_space = input_space.__name__
+    if callable(output_space):
+        output_space = output_space.__name__
+
+    total_space = ['erf', 'derf', 'log_erf', 'log_derf']
+
+    assert input_space in total_space
+    assert output_space in total_space
+    assert isinstance(data, np.ndarray)
+    assert threshold > 0.0
+
+    data_ndim = data.ndim
+    if data_ndim == 1:
+        data = data[None, :]
+
+    # thresholding the data in the linear space
+    if input_space in ['erf', 'derf']:
+        data = np.maximum(threshold, data)
+
+    if input_space == output_space:
+        output_data = data.copy()
+    elif output_space == 'log_' + input_space:
+        output_data = np.log(data)
+    elif input_space == 'log_' + output_space:
+        output_data = np.exp(data)
+    elif 'derf' in input_space:
+        if 'log' in input_space:
+            data = np.exp(data)
+        output_data = np.cumsum(data, axis=1)
+        if 'log' in output_space:
+            output_data = np.log(output_data)
+    else:
+        if 'log' in input_space:
+            data = np.exp(data)
+        output_data = data - np.insert(data[:, :-1], 0, 0.0)
+        if 'log' in output_space:
+            output_data = np.log(output_data)
+
+    # reverting the shape back if necessary
+    if data_ndim == 1:
+        output_data = output_data.ravel()
+
+    return output_data
