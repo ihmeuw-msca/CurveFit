46b981d60fe0a310ff1e4ab4590d28296a391e0e
jizezhang <jizez@uw.edu>
2020-04-10 11:42:49 -0700

    added result checker class in diagnostics

---
+++ src/curvefit/diagnostics/result_checkers.py
@@ -0,0 +1,42 @@
+import numpy as np
+import pandas as pd
+from curvefit.core.utils import split_by_group
+from general.diagnostics.baselines import LinearRegressionBaseline
+
+class ResultChecker:
+
+    def __init__(self, df, col_obs, col_est, col_group):
+        self.df = df
+        self.col_obs = col_obs
+        self.col_est = col_est 
+        self.col_group = col_group
+
+    def check_result(self, groups=None):
+        raise NotImplementedError() 
+
+
+class LogDerfLinearRegressionChecker(ResultChecker):
+
+    def __init__(self, df, col_log_derf_obs, col_est, est_space, col_group, col_t):
+        super().__init__(df, col_log_derf_obs, col_est, col_group)
+        self.est_space = est_space 
+        self.col_t = col_t
+
+    def check_result(self, groups=None): 
+        df_by_group = split_by_group(self.df, self.col_group)
+
+        log_derf_rmses = {}
+
+        for grp, df in df_by_group.items():
+            log_derf_obs = df[self.col_obs].to_numpy()
+            times = df[self.col_t].to_numpy()
+            estimates = df[self.col_est]
+            lr_log_derf = LinearRegressionBaseline([log_derf_obs], [grp], [times])
+            lr_log_derf.fit()
+            metric_fun = lambda est: np.linalg.norm(est - log_derf_obs)
+            log_derf_rmses.update(lr_log_derf.compare([estimates], [grp], metric_fun))
+        
+        result_df = pd.from_dict(log_derf_rmses, orient='index', columns=['curr model', 'baseline'])
+        return result_df
+
+    
--- src/general/diagnostics/baselines.py
+++ src/general/diagnostics/baselines.py
@@ -1,60 +1,53 @@
-from utils import check_array_dims
 from sklearn.linear_model import HuberRegressor
 
 class Baseline:
 
-    def __init__(self, observations, locations, features=None):
-        if len(observations) != len(locations) or len(locations) != len(features):
+    def __init__(self, observations, groups, features=None):
+        if len(observations) != len(groups) or len(groups) != len(features):
             raise ValueError()
         self.observations = observations 
-        self.locations = locations
+        self.groups = groups
         self.features = features
 
     def fit(self):
         raise NotImplementedError()
 
-    def is_better_than_baseline(self, estimations, locations, metric_fun=None):
+    def compare(self, estimations, groups, metric_fun):
         raise NotImplementedError()
 
-    def add_location(self, observation, location, feature=None):
+    def add_group(self, observation, group, feature=None):
         raise NotImplementedError()
 
 
 class LinearRegressionBaseline(Baseline):
 
-    def __init__(self, observations, locations, features):       
-        super().__init__(observations, locations, features) 
+    def __init__(self, observations, groups, features, regressor=HuberRegressor()):       
+        super().__init__(observations, groups, features) 
         for obs, ft in zip(observations, features):
             if len(obs) != len(ft):
                 raise ValueError()
-    
-    def fit(self):
+        self.regressor = regressor      
         self.baseline_est = {}
         self.models = {}
-        for loc, obs, ft in zip(self.locations, self.observations, self.features):
-            self.models[loc] = HuberRegressor()
-            self.models[loc].fit(ft, obs)
-            self.baseline_est[loc] = self.models[loc].predict(ft)
     
-    def is_better_than_baseline(self, estimations, locations, metric_fun=None, verbose=False):
-        if len(estimations) != len(locations):
+    def fit(self):
+        for grp, obs, ft in zip(self.groups, self.observations, self.features):
+            self.add_group(obs, grp, ft)
+    
+    def compare(self, estimations, groups, metric_fun):
+        # have two metric functions in case baseline and passed-in estimates are not in the same space
+        if len(estimations) != len(groups):
             raise ValueError()
-        is_smaller = {} # better in the sense of smaller metric fun value
         metric_fun_values = {}
-        if verbose:
-            print('location \t curr model \t baseline')
-        for loc, est in zip(locations, estimations):
-            metric_fun_values[loc] = (metric_fun(est), metric_fun(self.baseline_est[loc]))
-            is_smaller[loc] = metric_fun_values[loc][0] < metric_fun_values[loc][1]
-            if verbose:
-                print(loc, '\t', metric_fun_values[loc][0], '\t', metric_fun_values[loc][1])
-        return is_smaller, metric_fun_values
+        for grp, est in zip(groups, estimations):
+            metric_fun_values[grp] = (metric_fun(self.baseline_est[grp]), metric_fun(est))
+        return metric_fun_values
 
-    def add_location(self, observation, location, feature):
-        model = HuberRegressor()
+    def add_group(self, observation, group, feature):
+        model = self.regressor
         model.fit(feature, observation)
-        self.models[location] = model
-        self.baseline_est[location] = model.predict(feature)
+        self.models[group] = model
+        self.baseline_est[group] = model.predict(feature)
                 
 
 
