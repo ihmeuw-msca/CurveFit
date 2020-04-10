3f8b7050300768069adef83366a5e55f441e35fc
jizezhang <jizez@uw.edu>
2020-04-10 11:45:36 -0700

    fixed some typos

--- src/curvefit/diagnostics/result_checkers.py
+++ src/curvefit/diagnostics/result_checkers.py
@@ -17,14 +17,12 @@ class ResultChecker:
 
 class LogDerfLinearRegressionChecker(ResultChecker):
 
-    def __init__(self, df, col_log_derf_obs, col_est, est_space, col_group, col_t):
+    def __init__(self, df, col_log_derf_obs, col_est, col_group, col_t):
         super().__init__(df, col_log_derf_obs, col_est, col_group)
-        self.est_space = est_space 
         self.col_t = col_t
 
     def check_result(self, groups=None): 
         df_by_group = split_by_group(self.df, self.col_group)
-
         log_derf_rmses = {}
 
         for grp, df in df_by_group.items():
@@ -33,10 +31,10 @@ class LogDerfLinearRegressionChecker(ResultChecker):
             estimates = df[self.col_est]
             lr_log_derf = LinearRegressionBaseline([log_derf_obs], [grp], [times])
             lr_log_derf.fit()
-            metric_fun = lambda est: np.linalg.norm(est - log_derf_obs)
+            metric_fun = lambda est: np.mean((est - log_derf_obs)**2)
             log_derf_rmses.update(lr_log_derf.compare([estimates], [grp], metric_fun))
         
-        result_df = pd.from_dict(log_derf_rmses, orient='index', columns=['curr model', 'baseline'])
+        result_df = pd.from_dict(log_derf_rmses, orient='index', columns=['baseline', 'curr model'])
         return result_df
 
     
