58fe376c46de6294afb70da586b67a69137f27a0
Brad Bell <bradbell@seanet.com>
2020-04-12 16:46:52 -0700

    cppad_py: functions.py: change cppad_py.a_double -> a_double and only consider that case.

--- cppad_py/functions.py
+++ cppad_py/functions.py
@@ -1,29 +1,17 @@
 import numpy
-import cppad_py
 import scipy
+from cppad_py import a_double
 
-def erf(vec) :
-    assert isinstance(vec, numpy.ndarray)
-    assert vec.ndim == 1
-    #
-    if isinstance(vec[0], cppad_py.a_double) :
-        result = numpy.empty(len(vec), dtype = cppad_py.a_double )
-        for i in range( len(vec) ) :
-            result[i] = vec[i].erf()
-    else :
-        result = scipy.special.erf(vec)
+def a_erf(vec) :
+    result = numpy.empty(len(vec), dtype = a_double )
+    for i in range( len(vec) ) :
+        result[i] = vec[i].erf()
     return result
 #
-def exp(vec) :
-    assert isinstance(vec, numpy.ndarray)
-    assert vec.ndim == 1
-    #
-    if isinstance(vec[0], cppad_py.a_double) :
-        result = numpy.empty(len(vec), dtype = cppad_py.a_double )
-        for i in range( len(vec) ) :
-            result[i] = vec[i].exp()
-    else :
-        result = numpy.exp(vec)
+def a_exp(vec) :
+    result = numpy.empty(len(vec), dtype = a_double )
+    for i in range( len(vec) ) :
+        result[i] = vec[i].exp()
     return result
 #
 def unpack_param(t, param) :
@@ -31,6 +19,9 @@ def unpack_param(t, param) :
     assert t.ndim == 1
     if param.ndim == 2 :
         assert param.shape[1] == t.shape[0]
+    assert t.dtype == a_double
+    assert param.dtype == a_double
+    #
     alpha  = param[0]
     beta   = param[1]
     p      = param[2]
@@ -39,10 +30,10 @@ def unpack_param(t, param) :
 def gaussian_cdf(t, param) :
     alpha, beta, p = unpack_param(t, param)
     z      = alpha * (t - beta)
-    return p * ( cppad_py.a_double(1.0) + erf(z) ) / cppad_py.a_double(2.0)
+    return p * ( a_double(1.0) + a_erf(z) ) / a_double(2.0)
 #
 def expit(t, param) :
     alpha, beta, p = unpack_param(t, param)
     z      = alpha * (t - beta)
-    return p / ( cppad_py.a_double(1.0) + exp(-z) )
+    return p / ( a_double(1.0) + a_exp(-z) )
 #
