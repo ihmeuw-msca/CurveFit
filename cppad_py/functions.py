91261012ee8507b83e2395fa7057dcc7e9916658
Brad Bell <bradbell@seanet.com>
2020-04-12 14:10:20 -0700

    cppad_py: add expit to functions.py and test it.

--- cppad_py/functions.py
+++ cppad_py/functions.py
@@ -14,6 +14,18 @@ def erf(vec) :
         result = scipy.special.erf(vec)
     return result
 #
+def exp(vec) :
+    assert isinstance(vec, numpy.ndarray)
+    assert vec.ndim == 1
+    #
+    if isinstance(vec[0], cppad_py.a_double) :
+        result = numpy.empty(len(vec), dtype = cppad_py.a_double )
+        for i in range( len(vec) ) :
+            result[i] = vec[i].exp()
+    else :
+        result = numpy.exp(vec)
+    return result
+#
 def gaussian_cdf(t, param) :
     assert param.shape[0] == 3
     assert t.ndim == 1
@@ -24,3 +36,15 @@ def gaussian_cdf(t, param) :
     p      = param[2]
     z      = alpha * (t - beta)
     return p * ( cppad_py.a_double(1.0) + erf(z) ) / cppad_py.a_double(2.0)
+#
+def expit(t, param) :
+    assert param.shape[0] == 3
+    assert t.ndim == 1
+    if param.ndim == 2 :
+        assert param.shape[1] == t.shape[0]
+    alpha  = param[0]
+    beta   = param[1]
+    p      = param[2]
+    z      = alpha * (t - beta)
+    return p / ( cppad_py.a_double(1.0) + exp(-z) )
+#
