952860e2daf109967898b4d81db3dff37e0cea82
zhengp0 <zhengp@uw.edu>
2020-04-18 17:46:11 -0700

    update: add bounds to the gaussian mixture weight sum.

--- src/curvefit/core/gauss_mix.py
+++ src/curvefit/core/gauss_mix.py
@@ -41,21 +41,29 @@ class GaussianMixture:
                                                             self.params,
                                                             self.beta_stride,
                                                             self.mixture_size)
+        self.nu = 1e-2
         self.weights = None
         self.result = None
+        self.sum_weights_bounds = None
 
     def objective(self, w):
-        residual = (self.obs - self.mat.dot(w))/self.obs_se
-        return 0.5*np.sum(residual**2)
+        residual = (self.obs - self.mat.dot(w[:-1]))/self.obs_se
+        sum_residual = (np.sum(w[:-1]) - w[-1])/self.nu
+        val = 0.5*np.sum(residual**2) + 0.5*np.sum(sum_residual**2)
+        return val
 
     def gradient(self, w):
-        residual = (self.obs - self.mat.dot(w))/self.obs_se
-        return -(self.mat.T/self.obs_se).dot(residual)
+        residual = (self.obs - self.mat.dot(w[:-1]))/self.obs_se
+        sum_residual = (np.sum(w[:-1]) - w[-1])/self.nu
+        grad = -(self.mat.T/self.obs_se).dot(residual) + sum_residual/self.nu
+        grad = np.hstack((grad, -sum_residual/self.nu))
+        return grad
 
     def fit_mixture_weights(self, w0=None, bounds=None, options=None):
         if w0 is None:
             w0 = np.zeros(self.mixture_size)
             w0[self.mixture_size//2] = 1.0
+            w0 = np.hstack((w0, 1.0))
 
         if bounds is None:
             bounds = np.array([[0.0, np.inf]]*self.mixture_size)
@@ -69,7 +77,7 @@ class GaussianMixture:
             options=options
         )
         self.result = result
-        self.weights = self.result.x
+        self.weights = self.result.x[:-1]
 
     def predict(self, t):
         mat, _ = utils.compute_gaussian_mixture_matrix(t,
