11bcca82977e081d44dcf55b75cb6f9a22ca34d1
Marlena Bannick <mnorwood@uw.edu>
2020-04-22 15:58:42 -0700

    added parameterset tests.

--- tests/core/test_parameter.py
+++ tests/core/test_parameter.py
@@ -1,4 +1,5 @@
 import numpy as np
+import pytest
 
 from curvefit.core.parameter import Parameter
 from curvefit.core.parameter import ParameterSet
@@ -57,13 +58,49 @@ def test_parameter_bounds():
     assert parameter.re_bounds[0] == [-3., 1.]
 
 
-def test_parameter_set():
-    pass
+@pytest.fixture
+def parameter1():
+    return Parameter(
+        param_name='alpha', link_fun=lambda x: x,
+        var_link_fun=[lambda x: x], covariates=['covariate1'],
+        fe_init=[0.], re_init=[0.]
+    )
 
 
-def test_parameter_set_no_function():
-    pass
+@pytest.fixture
+def parameter2():
+    return Parameter(
+        param_name='beta', link_fun=lambda x: x,
+        var_link_fun=[lambda x: x], covariates=['covariate2'],
+        fe_init=[0.], re_init=[0.]
+    )
 
 
-def test_parameter_set_functional_prior():
-    pass
+def test_parameter_set(parameter1, parameter2):
+    param_set = ParameterSet(
+        parameter_list=[parameter1, parameter2],
+        parameter_functions=[lambda params: params[0] * params[1]],
+    )
+    assert len(param_set.parameter_list) == 2
+    assert param_set.num_params == 2
+    assert param_set.num_fe == 2
+    assert len(param_set.parameter_functions) == 1
+    assert len(param_set.parameter_function_priors) == 1
+    assert param_set.parameter_function_priors[0] == [0., np.inf]
+
+
+def test_parameter_set_no_function(parameter1, parameter2):
+    with pytest.raises(RuntimeError):
+        ParameterSet(
+            parameter_list=[parameter1, parameter2],
+            parameter_function_priors=[[0.0, np.inf]]
+        )
+
+
+def test_parameter_set_functional_prior(parameter1, parameter2):
+    param_set = ParameterSet(
+        parameter_list=[parameter1, parameter2],
+        parameter_functions=[lambda params: params[0] * params[1]],
+        parameter_function_priors=[[0.1, 1.0]]
+    )
+    assert param_set.parameter_function_priors[0] == [0.1, 1.0]
