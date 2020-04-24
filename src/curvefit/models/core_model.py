a6b95f4373e1dc6d156a734e16fe7eae5f59d5fb
jizezhang <jizez@uw.edu>
2020-04-24 11:48:27 -0700

    added core model file

---
+++ src/curvefit/models/core_model.py
@@ -0,0 +1,39 @@
+from functools import reduce
+from operator import iconcat
+import numpy as np
+from curvefit.core.objective_fun import objective_fun
+
+class Model:
+
+    def __init__(self, param_set, curve_fun, loss_fun):
+        self.param_set = param_set
+        self.curve_fun = curve_fun
+        self.loss_fun = loss_fun
+
+    def objective(self, x, data):
+        df = data[0]
+        data_specs = data[1]
+        t = df[data_specs.col_t].to_numpy()
+        obs = df[data_specs.col_obs].to_numpy()
+        obs_se = df[data_specs.col_obs_se].to_numpy()
+        covs_mat = []
+        for covs in self.param_set.covariate:
+            covs_mat.append(df[covs].to_numpy())
+        group_sizes = [df.shape[0]]
+        fe_gprior = np.array(reduce(iconcat, self.param_set.fe_gprior, []))
+
+        return objective_fun(
+            x, 
+            t, 
+            obs, 
+            obs_se, 
+            covs_mat, 
+            group_sizes, 
+            self.curve_fun, 
+            self.loss_fun,
+            self.param_set.link_fun,
+            self.param_set.var_link_fun,
+            fe_gprior,
+            self.param_set.re_gprior,
+            self.param_set.parameter_functions,
+        )
