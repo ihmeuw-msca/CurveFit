3dea233f69c7698d6d614baf5ca949472a67ef26
jizezhang <jizez@uw.edu>
2020-04-24 21:36:41 -0700

    added gradient for core model

--- src/curvefit/models/core_model.py
+++ src/curvefit/models/core_model.py
@@ -174,6 +174,20 @@ class Model:
             param_gprior=self.data_inputs.param_gprior_info,
         ) 
 
+    def gradient(self, x, data, eps=1e-16):
+        if self.data_inputs is None:
+            self.convert_inputs(data)
+        finfo = np.finfo(float)
+        step  = finfo.tiny / finfo.eps
+        x_c = x + 0j
+        grad = np.zeros(x.size)
+        for i in range(x.size):
+            x_c[i] += step*1j
+            grad[i] = self.objective(x_c, data).imag/step
+            x_c[i] -= step*1j
+
+        return grad
+
     def forward(self, x, t):
         params = effects2params(
             x, 
