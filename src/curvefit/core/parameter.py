b85b3629677f14ae95f3c5b9c5a8ce819f66abbb
jizezhang <jizez@uw.edu>
2020-04-25 21:54:51 -0700

    Revert "make list of variables and parameters visible so it might be easier to update priors for an parameter set"
    This reverts commit 37d9b807277cebb98f24808191aacc71b924205e.

--- src/curvefit/core/parameter.py
+++ src/curvefit/core/parameter.py
@@ -123,7 +123,7 @@ class Parameter:
 
     param_name: str
     link_fun: Callable
-    variables: List[Variable]
+    variables: InitVar[List[Variable]]
 
     num_fe: int = field(init=False)
     covariate: List[str] = field(init=False)
@@ -135,12 +135,12 @@ class Parameter:
     fe_bounds: List[List[float]] = field(init=False)
     re_bounds: List[List[float]] = field(init=False)
 
-    def __post_init__(self):
-        assert isinstance(self.variables, list)
-        assert len(self.variables) > 0
-        assert isinstance(self.variables[0], Variable)
-        self.num_fe = len(self.variables)
-        for k, v in consolidate(Variable, self.variables).items():
+    def __post_init__(self, variables):
+        assert isinstance(variables, list)
+        assert len(variables) > 0
+        assert isinstance(variables[0], Variable)
+        self.num_fe = len(variables)
+        for k, v in consolidate(Variable, variables).items():
             self.__setattr__(k, v)
 
 
@@ -238,7 +238,7 @@ class ParameterSet(Prototype):
 
     def __post_init__(self, parameters, parameter_functions):
 
-        for k, v in consolidate(Parameter, self.parameters, exclude=['num_fe']).items():
+        for k, v in consolidate(Parameter, parameters, exclude=['num_fe']).items():
             self.__setattr__(k, v)
 
         for k, v in consolidate(ParameterFunction, parameter_functions).items():
@@ -250,7 +250,7 @@ class ParameterSet(Prototype):
             raise RuntimeError("Cannot have duplicate parameter functions in a set.")
 
         self.num_fe = 0
-        for param in self.parameters:
+        for param in parameters:
             self.num_fe += param.num_fe
 
     def get_param_index(self, param_name):
