13a4495d2d2b089bb27e4b918bcd85f727d92142
Marlena Bannick <mnorwood@uw.edu>
2020-04-27 14:30:57 -0700

    Adding model run

---
+++ src/curvefit/run/model_run.py
@@ -0,0 +1,81 @@
+
+
+class ModelRunner:
+    """
+    {begin_markdown ModelRunner}
+    {spell_markdown }
+
+    # `curvefit.run.model_run.ModelRunner`
+    ## Runs a full model from start to finish for all groups
+
+    Runs a full model, including initialization for model priors,
+    predictive validity, and creating draws.
+
+    ## Arguments
+
+    - `prior_initializer (curvefit.initializer.initializers.PriorInitializer)`: prior initializer
+    - `initializer_data (curvefit.core.data.Data)`: data for the initializer only,
+        might be the same or different from `data` (could overlap, could be completely separate)
+    - `data (curvefit.core.data.Data)`: data object with all groups
+    - `model (curvefit.models.base.Model)`: a model instance with an attached
+        ParameterSet
+    - `solver (curvefit.solvers.solver.Solver)`: a solver instance to fit the model
+    - `predictive_validity (curvefit.uncertainty.predictive_validity.PredictiveValidity)`: a predictive validity object
+    - `residual_model (curvefit.uncertainty.residual_models._ResidualModel)`: a residual model
+    - `draws (curvefit.uncertainty.draws.Draws)`: a draws object
+
+    ## Methods
+
+    ### `run`
+    The main run function for a `ModelRunner`. This function initializes the priors,
+    it runs predictive validity, uses the results of predictive validity
+    to estimate residuals, and creates draws by simulating them forwards in time.
+
+    ## Examples
+
+
+    {end_markdown ModelRunner}
+    """
+    def __init__(self, prior_initializer, initializer_data,
+                 data, model, solver,
+                 predictive_validity, residual_model, draws):
+
+        self.prior_initializer = prior_initializer
+        self.initializer_data = initializer_data
+
+        self.data = data
+        self.model = model
+
+        self.solver = solver
+        if not hasattr(self.model, 'param_set'):
+            raise RuntimeError("The model instance must have a parameter set.")
+        self.model.erase_data()
+        self.solver.detach_model_instance()
+
+        self.predictive_validity = predictive_validity
+        self.residual_model = residual_model
+        self.draws = draws
+
+    def run(self):
+
+        # Initialize the priors
+        self.model.parameter_set = self.prior_initializer.initialize(
+            data=self.initializer_data,
+            model_prototype=self.model,
+            solver_prototype=self.solver
+        )
+        # Run predictive validity
+        self.predictive_validity.run_predictive_validity(
+            data=self.data,
+            model_prototype=self.model,
+            solver_prototype=self.solver
+        )
+        # Use the results of the predictive validity analysis to estimate residuals
+        self.residual_model.fit_residuals(
+            residual_df=self.predictive_validity.get_residual_data()
+        )
+        # Create draws by simulating forward residuals
+        self.draws.create_draws(
+            residual_model=self.residual_model,
+            theta=self.predictive_validity.theta
+        )
