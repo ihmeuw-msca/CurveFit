dac9bf19c8e1bf0f7f8c9630604803e2edf42854
jizezhang <jizez@uw.edu>
2020-04-27 17:50:42 -0700

    added multi init for core model

--- tests/solvers/test_solvers.py
+++ tests/solvers/test_solvers.py
@@ -44,12 +44,12 @@ class TestBaseSolvers:
         assert np.abs(solver.fun_val_opt) < 1e-5
 
     @pytest.mark.parametrize('curve_fun', [ln_gaussian_pdf, ln_gaussian_cdf, gaussian_pdf, gaussian_cdf])
-    def test_scipyopt_core_mdoel(self, curve_fun):
+    def test_scipyopt_core_model(self, curve_fun):
         params_set, params_true, _ = simulate_params(1)
         data = simulate_data(curve_fun, params_true)
         core_model = CoreModel(params_set, curve_fun, normal_loss)
         solver = ScipyOpt(core_model)
-        solver.fit(data=data, options={'maxiter': 50})
+        solver.fit(data=data, options={'maxiter': 200})
         y_pred = solver.predict(t=data[0]['t'].to_numpy())
         y_true = data[0]['obs'].to_numpy()
         assert np.linalg.norm(y_pred - y_true) / np.linalg.norm(y_true) < 1e-2
@@ -74,6 +74,25 @@ class TestCompositeSolvers:
         for x in xs_init:
             assert rb.objective(x, None) >= solver.fun_val_opt
 
+    @pytest.mark.parametrize('curve_fun', [ln_gaussian_pdf, ln_gaussian_cdf, gaussian_pdf, gaussian_cdf])
+    def test_multi_init_core_model(self, curve_fun):
+        params_set, params_true, x_true = simulate_params(1)
+        data = simulate_data(curve_fun, params_true)
+        core_model = CoreModel(params_set, curve_fun, normal_loss)
+
+        num_init = 5
+        xs_init = np.random.randn(num_init, x_true.shape[1]* 2)
+        sample_fun = lambda x: xs_init
+        solver = MultipleInitializations(sample_fun)
+        solver.set_model_instance(core_model)
+        solver.fit(data=data, options={'maxiter': 200})
+        y_pred = solver.predict(t=data[0]['t'].to_numpy())
+        y_true = data[0]['obs'].to_numpy()
+        assert np.linalg.norm(y_pred - y_true) / np.linalg.norm(y_true) < 1e-2
+
+        for x in xs_init:
+            assert core_model.objective(x, None) >= solver.fun_val_opt
+
     def test_gaussian_mixture_integration(self):
         gm_model = GaussianMixtures(stride=1.0, size=3)
         params_set, params_true, _ = simulate_params(1)
@@ -93,8 +112,8 @@ class TestCompositeSolvers:
         
         assert np.linalg.norm(y_pred - y_true) < np.linalg.norm(y_pred_base - y_true)
 
-    def test_smart_initialization(self):
-        params_set, params_true, x_true = simulate_params(3)
+    def test_smart_initialization_run(self):
+        params_set, params_true, _ = simulate_params(3)
         data = simulate_data(gaussian_pdf, params_true)
         core_model = CoreModel(params_set, gaussian_pdf, normal_loss)
 
