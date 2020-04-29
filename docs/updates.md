1e05ab44fefe594612aeb9ce268237e8c3d1b69f
Marlena Bannick <mnorwood@uw.edu>
2020-04-29 07:49:01 -0700

    Reformatting.

---
+++ docs/updates.md
@@ -0,0 +1,4 @@
+# Release Notes
+
+- 1.0.0: Large refactor of code structure
+- 0.0.0 ()
--- example/unzip_x.py
+++ example/unzip_x.py
@@ -1,5 +1,6 @@
 #! /usr/bin/env python3
-'''{begin_markdown unzip_x_xam}
+"""
+{begin_markdown unzip_x_xam}
 {spell_markdown params ndim}
 
 # Example and Test of unzip_x
@@ -8,26 +9,28 @@
 [unzip_x](unzip_x.md)
 
 ## Example Source Code
-```python'''
-import sys
+```python """
 import numpy
-import sandbox
-sandbox.path()
-import curvefit
-#
+from curvefit.core.effects2params import unzip_x
+
+
 num_groups = 2
-num_fe     = 3
-x          = numpy.array( range( (num_groups + 1) * num_fe ) )
-fe, re     = curvefit.core.effects2params.unzip_x(x, num_groups, num_fe)
+num_fe = 3
+x = numpy.array(range((num_groups + 1) * num_fe))
+fe, re = unzip_x(x, num_groups, num_fe)
+
 assert fe.ndim == 1
 assert re.ndim == 2
 assert fe.shape[0] == num_fe
 assert re.shape[0] == num_groups
 assert re.shape[1] == num_fe
-assert all( fe == x[0 : num_fe] )
-for i in range(num_groups) :
-    assert all( re[i,:] == x[(i+1)*num_fe : (i+2)*num_fe] )
+assert all(fe == x[0: num_fe])
+
+for i in range(num_groups):
+    assert all(re[i, :] == x[(i + 1) * num_fe: (i + 2) * num_fe])
+
 print('unzip_x.py: OK')
-sys.exit(0)
-'''```
-{end_markdown unzip_x_xam}'''
+
+"""```
+{end_markdown unzip_x_xam}
+"""
