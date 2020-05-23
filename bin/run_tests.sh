11a8f34f8368e684d84a9aa590c40149a6b47b9a
Brad Bell <bradbell@seanet.com>
2020-05-22 22:32:35 -0700

    refactor: run_tests.sh: correct location of get_cppad.sh.

--- bin/run_tests.sh
+++ bin/run_tests.sh
@@ -1,7 +1,9 @@
 #! /bin/bash -e
 # This is a test wrapper used by .travis.yml to set LD_LIBRARY_PATH
 # -----------------------------------------------------------------------------
-eval $(sed -n '/^cppad_prefix=/p'  bin/get_cppad.sh)
+get_cppad_sh='build/external/cppad_py.git/bin/get_cppad.sh'
+eval $(sed -n '/^cppad_prefix=/p'  $get_cppad_sh)
+echo "export LD_LIBRARY_PATH=$cppad_prefix/lib:$LD_LIBRARY_PATH"
 export LD_LIBRARY_PATH="$cppad_prefix/lib:$LD_LIBRARY_PATH"
 make tests
 make examples
