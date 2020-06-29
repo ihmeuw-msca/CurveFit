13c758ce8db49b00595bf2a51d593fdfc67ab89a
Marlena Bannick <mnorwood@uw.edu>
2020-06-29 11:31:43 -0700

    Upped version.

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import find_packages
 
 setup(
     name='curvefit',
-    version='0.0.0',
+    version='1.0.0',
     description='Curve Fitting Tool',
     url='https://github.com/ihmeuw-msca/CurveFit',
     license='MIT',
